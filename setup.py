12fa7446bf6263ab5dcb8765313fe4ea05fc3cfd
Chris Tozer <christopherjtozer@gmail.com>
2018-05-14 21:11:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cliciv',
+      version='0.1',
+      description='CLI game of civilisation-buildingg',
+      author='Chris Tozer',
+      author_email='christopherjtozer@gmail.com',
+      url='https://github.com/CJTozer/cliciv',
+      packages=['cliciv'],
+      scripts=['scripts/cliciv'],
+     )
