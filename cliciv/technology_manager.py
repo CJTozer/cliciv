a7bb783a26657150b461c12ccfa4fe4755c42f3e
Chris Tozer <christopherjtozer@gmail.com>
2018-05-19 13:13:00 +0100

    Research unlock trigger

--- cliciv/technology_manager.py
+++ cliciv/technology_manager.py
@@ -4,6 +4,7 @@ from typing import List
 from thespian.actors import Actor, ActorExitRequest
 
 from cliciv.messages import RegisterForUpdates, TechnologyNewState, InitialState, TechProduced
+from cliciv.utils.data import dict_from_data
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +13,7 @@ class TechnologyManager(Actor):
     def __init__(self):
         self.registered: List[str] = []
         self.technology_state = None
+        self.tech_tree = dict_from_data('technology')
         super(TechnologyManager, self).__init__()
 
     def receiveMessage(self, msg, sender: str):
@@ -26,12 +28,35 @@ class TechnologyManager(Actor):
                 self.registered.append(sender)
             self.send(sender, TechnologyNewState(self.technology_state))
         elif isinstance(msg, TechProduced):
-            old_amount = self.technology_state.research_accrued.get(msg.area)
-            self.technology_state.research_accrued[msg.area] = old_amount
+            old_amount = self.technology_state.research_accrued.get(msg.area, 0)
+            self.technology_state.research_accrued[msg.area] = old_amount + msg.amount
+            self.check_for_unlocked_tech()
         else:
             logger.error("Ignoring unexpected message: {}".format(msg))
+
+    def notify_all(self):
+        for r in self.registered:
+            self.send(r, TechnologyNewState(self.technology_state))
+
+    def check_for_unlocked_tech(self):
+        unlocked_research = []
+        for tech, tech_info in self.tech_tree.items():
+            if self.technology_state.satisfies(tech_info['requires']):
+                unlocked_research.append(tech)
+        if set(unlocked_research) != set(self.technology_state.unlocked_research):
+            logger.info("New research unlocked: {}".format(
+                [t for t in unlocked_research if t not in self.technology_state.unlocked_research]
+            ))
+            self.notify_all()
 
 
 class TechnologyState(object):
     def __init__(self, data):
         self.__dict__ = data
+
+    def satisfies(self, required):
+        # Check whether enough tech has been accrued
+        return all([
+            self.research_accrued.get(k, 0) >= v
+            for k, v in required['research_accrued'].items()
+        ])
